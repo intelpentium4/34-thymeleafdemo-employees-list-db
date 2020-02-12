5bde22ca6763cf07304815572489ddbee99db695
intelnuc <intel.darren@gmail.com>
2020-02-11 21:45:29 -0500

    Implemented functionality to 'Add Employee' button

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,13 +1,12 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
@@ -28,21 +27,31 @@ public class EmployeeController {
 	public String listEmployees(Model theModel) {
 		
 		// get employees from db
-		List<Employee> theEmployees = employeeService.findAll();
+		List<Employee> theEmployee = employeeService.findAll();
 		
 		// add to the spring model
-		theModel.addAttribute("employees", theEmployees);
+		theModel.addAttribute("employee", theEmployee);
 		
 		return "employees/list-employees";
 	}
 	
 	@GetMapping("/showFormForAdd")
-	public String showFormForAdd(Model theModel) {
+	public String showAddEmployeeForm(Model theModel) {
 		
 		// create model attribute to bind the form data
 		Employee theEmployee = new Employee();
 		theModel.addAttribute("employee", theEmployee);
 		
 		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		// save the employee
+		employeeService.save(theEmployee);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
 	}
 }
